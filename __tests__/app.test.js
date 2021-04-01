de40e6a6ac4aec23ab6755d69a6aa2940beee552
Casey Warren <caseywar1017@gmail.com>
2021-04-01 18:15:47 -0400

    get all profiles routes and tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,18 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const ProfileService = require('../lib/services/ProfileService')
+const Profile = require('../lib/models/Profile')
 
 describe('profile routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  let profile;
+  beforeEach(async () => {
+    profile = await Profile.insert({ userName: 'test user', favoriteCharacter: '030555b3-4c92-4fce-93fb-e70c3ae3df8b', hairColor: 'Brown' });
+
   });
 
   it('creates a profile with the characters hair color', async () => {
@@ -15,9 +23,38 @@ describe('profile routes', () => {
 
     expect(res.body).toEqual({
       id: expect.any(String),
-    userName: 'test user',
+      userName: 'test user',
       favoriteCharacter: '030555b3-4c92-4fce-93fb-e70c3ae3df8b',
       hairColor: expect.any(String),
     });
+  });
+  
+
+  it('creates a profile with the characters hair color', async () => {
+    const res = await request(app)
+      .post('/api/v1/profiles')
+      .send({ userName: 'test user 2', favoriteCharacter: '3bc0b41e-3569-4d20-ae73-2da329bf0786', hairColor: 'Peach' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      userName: 'test user 2',
+      favoriteCharacter: '3bc0b41e-3569-4d20-ae73-2da329bf0786',
+      hairColor: expect.any(String),
+    });
+  });
+
+
+  it('gets all of the users', async () => {
+    // await request(app).post('/api/v1/profiles').send({ userName: 'test user', favoriteCharacter: ;'030555b3-4c92-4fce-93fb-e70c3ae3df8b' })
+
+    const res = await request(app)
+    .get(`/api/v1/profiles`);
+    expect(res.body).toEqual([{
+      id: expect.any(String),
+      userName: expect.any(String),
+      favoriteCharacter: expect.any(String),
+      hairColor: expect.any(String),
+    }
+]);
   });
 });
